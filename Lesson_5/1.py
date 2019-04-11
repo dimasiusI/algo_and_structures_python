249fa87f1a836347c5a1f762eb69c16b0cac0336
dimasiusI <dimasius93@gmail.com>
2019-04-11 23:04:31 +0300

    done

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,50 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from random import randint
+
+count = 4 #int(input('Сколько предприятий будем сравнивать?'))
+names= ['vbn', 'fgh', 'rtw', 'usb'] #чтобы не писать каждый раз в ручную
+
+all_pred = {}
+
+def add_pred():
+    pred = {1:0, 2:0, 3:0, 4:0}
+    for i in range(1,5):
+        pred[i] = randint(10, 50) #int(input(f'Прибыль за {i+1}-й месяц: '))
+
+    return pred
+
+
+for i in range(count):
+    name = names[i] #input('Name: ')
+    all_pred[name] = add_pred()
+
+print(all_pred)
+
+obshak = 0
+
+for i in all_pred.values():
+    for j in i.values():
+        obshak += j
+
+print(f'Средняя прибыль равна {int(obshak/count)}')
+
+new_all_pred = {}
+
+for i in all_pred:
+    srednyaya = 0
+    for j in all_pred[i].values():
+        srednyaya += j
+    new_all_pred[i] = srednyaya
+
+print(new_all_pred)
+
+for i in new_all_pred:
+    j = new_all_pred[i]
+    if j > obshak/count:
+        print(f'Предприятие {i} имеет доход выше среднего {j}')
+    else:
+        print(f'Предприятие {i} имеет доход ниже среднего {j}')
+
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,129 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+def summ (a,b):
+
+    #по умолчанию b > a
+    if len(a)>len(b):
+        a, b = b, a
+
+    while len(a) != len(b):
+        a.insert(0, '0')
+
+    a.reverse()
+    b.reverse()
+
+    sum_plus = 0
+    res = []
+    i = 0
+    while i < len(a):
+        sum_ = chisla[a[i]] + chisla[b[i]]
+        if sum_plus == 1:
+            sum_ += 1
+
+        if sum_ == 16:
+            sum_plus = 1
+            res.append('0')
+
+        if sum_ > 16:
+            sum_plus = sum_% 16
+            res.append(znachenie[sum_plus])
+            sum_plus = 1
+        elif sum_ < 16:
+            res.append(znachenie[sum_])
+            sum_plus = 0
+
+        i += 1
+
+    res.reverse()
+    a.reverse()
+    b.reverse()
+
+    return res
+
+def help_func_mult(a, b):
+    sum_plus = 0
+    res = []
+    for j in a:
+        mult_ = chisla[j] * chisla[b]
+
+        if sum_plus >= 1:
+            mult_ += sum_plus
+
+        if mult_ == 16:
+            sum_plus = 1
+            res.append('0')
+
+        if mult_ > 16:
+            add_plus = mult_ % 16
+            res.append(znachenie[add_plus])
+            sum_plus = mult_ // 16
+        elif mult_ < 16:
+            res.append(znachenie[mult_])
+            sum_plus = 0
+
+        if sum_plus > 1 and len(res) == len(a):
+            res.append(znachenie[sum_plus])
+            return res
+            break
+
+    return res
+
+def mult (a, b):
+    a.reverse()
+    b.reverse()
+
+    #по умолчанию b > a
+    if len(a)>len(b):
+        a, b = b, a
+
+    prom_res = []
+
+
+    razryad = 0
+    while razryad < len(b):
+        res = help_func_mult(a, b[razryad])
+        res.reverse()
+
+        if razryad >= 1:
+            i = 0
+            while i < razryad:
+                res.append('0')
+                i += 1
+
+        prom_res.append(res)
+
+        razryad += 1
+
+    #print(prom_res) #[‘7’, ‘C’, ‘9’, ‘F’, ‘E’]
+
+    while 1 != len(prom_res):
+        c = prom_res[0]
+        d = prom_res[1]
+        prom_res[0] = summ(c, d)
+        prom_res.pop(1)
+
+
+    a.reverse()
+    b.reverse()
+
+    return prom_res[0]
+
+
+
+a = 'A2'
+b = 'C4F'
+
+a = list(a)
+b = list(b)
+
+chisla = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15}
+znachenie = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'}
+
+
+print(f'Умножение двух шестнадцатеричных чисел {"".join(a)} и {"".join(b)} равняется {"".join(mult(a, b))}')
+print(f'Сумма двух шестнадцатеричных чисел {"".join(a)} и {"".join(b)} равняется {"".join(summ(a, b))}')
+
+
+
