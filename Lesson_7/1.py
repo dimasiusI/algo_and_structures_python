cdb47d1132fd26a5762737dcb951352eebf70c83
dimasiusI <dimasius93@gmail.com>
2019-05-29 17:03:15 +0300

    done

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,28 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+import random
+
+def bubble (arr):
+    count = 1
+    globalcount = 0 #Сколько раз пройдет цикл, чтобы достичь результата
+
+    while count != 0:
+        if count > 0:
+            count = 0
+        for i in range(len(arr)-1):
+            if arr[i] < arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                count += 1
+
+        globalcount += 1
+
+    return arr, globalcount
+
+
+n = random.sample(range(-100, 100), 25)
+
+print(n)
+
+print(bubble(n))
--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,46 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+import random
+
+def merge(arr):
+    if len(arr)>1:
+        mid = len(arr)//2
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
+
+        merge(lefthalf)
+        merge(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i<len(lefthalf) and j<len(righthalf):
+            if lefthalf[i]<righthalf[j]:
+                    arr[k]=lefthalf[i]
+                    i=i+1
+            else:
+                arr[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+            while i<len(lefthalf):
+                arr[k]=lefthalf[i]
+                i=i+1
+                k=k+1
+
+            while j<len(righthalf):
+                arr[k]=righthalf[j]
+                j=j+1
+                k=k+1
+
+    return arr
+
+
+n = random.sample(range(0, 50), 25)
+
+print(n)
+
+merge(n)
+
+print(n)
